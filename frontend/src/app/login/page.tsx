1aef839a927dad70e8d15604cf8a06b734952676
jjuuniper <elpicsid@gmail.com>
2026-01-06 16:45:44 -0600

    update login page to test dev branch auto deploy

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -6,7 +6,7 @@ const Page = () => (
     <div className="container page">
       <div className="row">
         <div className="col-md-6 offset-md-3 col-xs-12">
-          <h1 className="text-xs-center">Sign in 😎</h1>
+          <h1 className="text-xs-center">Sign in 😎🔫</h1>
           <p className="text-xs-center">
             <Link href="/register">Need an account?</Link>
           </p>
